be963af16872f1358453faa90bc0c4fc425410d8
Paulo Henrique Corrêa Lemos <=>
2022-08-22 16:05:36 -0400

    Commit do projeto que irei usar como base para projetos futuros

---
+++ src/sentry_object.js
@@ -0,0 +1,8 @@
+export class SentryObject {
+    constructor(id, name, yearMin, yearMax) {
+        this.id = id;
+        this.name = name;
+        this.yearMin = yearMin;
+        this.yearMax = yearMax;
+    }
+}
---
+++ src/sentry_service.js
@@ -0,0 +1,6 @@
+export async function getSentryObjects() {
+    const response = await fetch('https://api.nasa.gov/neo/rest/v1/neo/sentry?api_key=DEMO_KEY');
+    const result = await response.json();
+    return result['sentry_objects'];
+
+}
